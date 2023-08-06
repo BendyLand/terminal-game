c13814768efe5894a6d3fd2d6244a18589551988
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-08-06 15:57:55 -0400

    Update planning.py
    Implement check win for rows and columns.

--- planning.py
+++ planning.py
@@ -3,7 +3,7 @@
 # Build a terminal program using Python
 #! Add at least one interactive feature using input()
 #! Use Git version control
-# Use the command line and file navigation
+#! Use the command line and file navigation
 # Write a technical blog post on the project
 '''
 
@@ -16,6 +16,32 @@ row1 = ['_', '_', '_', '_', '_', '_', '_']
 
 p = [row1, row2, row3, row4, row5, row6]
 
+def check_win(turn, choice):
+    # check rows
+    for row in p:
+        tokens = 0
+        for item in row:
+            if item == player_token:
+                tokens += 1
+            if item != player_token:
+                tokens = 0
+            if tokens == 4:
+                print(f"Game over!\n{turn} wins!")
+                return True
+
+    # check columns
+    col_tokens = 0
+    for row in p:
+        if row[choice] == player_token:
+            col_tokens += 1
+        if row[choice] != player_token:
+            col_tokens = 0
+        if col_tokens == 4:
+            print(f"Game over!\n{turn} wins!")
+            return True
+
+    return False
+
 def display_board():
     r6 = f"|{'|'.join(row6)}|"
     r5 = f"|{'|'.join(row5)}|"
@@ -26,38 +52,40 @@ def display_board():
 
     print(f"\n\t{r6}\n\t{r5}\n\t{r4}\n\t{r3}\n\t{r2}\n\t{r1}\n\t 1 2 3 4 5 6 7\n")
 
-print("Welcome to Connect Four!")
 player_one = input("Please enter a name for player 1: ") or "Player 1"
 player_two = input("Please enter a name for player 2: ") or "Player 2"
 player_turn = player_one
+player_token = 'x' if player_turn == player_one else 'o'
+
 def play_round():
-    i = 0
-    while True:
+    game_won = False
+    while not game_won:
         global player_turn
         player_token = 'x' if player_turn == player_one else 'o'
         print(f"It's {player_turn}'s ({player_token}) turn!")
         print("Here is the current board: ")
         display_board()
-        
-        player_choice = 0  
-        while not (1 <= player_choice <= 7):
-            player_choice = int(input("Please select a column to play your token: ")) - 1
-            if not (1 <= player_choice <= 7):
+
+        player_choice = -1
+        while not (0 <= player_choice <= 7):
+            player_choice = int(input("Please select a column to play your token (1 - 7): ")) - 1
+            if not (0 <= player_choice <= 6):
                 print("Invalid input, please try again!\n")
 
-        # dynamic check to find the row
         for row in p:
             if row[player_choice] == '_':
                 row[player_choice] = player_token
+                game_won = check_win(player_turn, player_choice)
                 break
             if row6[player_choice] != '_':
                 print('Column is full, pick another one!\n')
                 play_round()
-        
+
         display_board()
         player_turn = player_one if player_turn == player_two else player_two
-        i += 1
-        if i > 10:
-            break
 
+
+print("\nWelcome to Connect Four!")
 play_round()
+
+

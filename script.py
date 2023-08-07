9c7fb3272ec7e8b8cccd32434f9e72de6cf297f7
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-08-07 11:36:36 -0400

    Add check win function to script file.

--- script.py
+++ script.py
@@ -6,3 +6,68 @@ row2 = ['_', '_', '_', '_', '_', '_', '_']
 row1 = ['_', '_', '_', '_', '_', '_', '_']
 
 p = [row1, row2, row3, row4, row5, row6]
+
+def check_win(turn, choice, p_token):
+    def check_diagonal_win(tokens):
+        if tokens >= 4:
+            print(f"Game over!\n{turn} wins!")
+            return True
+        return False
+
+    # check rows
+    for row in p:
+        tokens = 0
+        for item in row:
+            if item == p_token:
+                tokens += 1
+                if tokens >= 4:
+                    print(f"Game over!\n{turn} wins!")
+                    return True
+            else:
+                tokens = 0
+
+    # check columns
+    col_tokens = 0
+    for row in p:
+        if row[choice] == p_token:
+            col_tokens += 1
+            if col_tokens >= 4:
+                print(f"Game over!\n{turn} wins!")
+                return True
+        else:
+            col_tokens = 0
+
+    # check diagonal (bottom left to top right)
+    r_index, c_index = 0, 0
+    for row in range(len(p) - 1, -1, -1):
+        for col in range(len(p[row])):
+            if p[row][col] == p_token:
+                tokens = 1
+                r_index, c_index = row, col
+                while r_index - 1 >= 0 and c_index + 1 < len(p[0]):
+                    r_index -= 1
+                    c_index += 1
+                    if p[r_index][c_index] == p_token:
+                        tokens += 1
+                        if check_diagonal_win(tokens):
+                            return True
+                    else:
+                        break
+
+    # check diagonal (bottom right to top left)
+    for row in range(len(p) - 1, -1, -1):
+        for col in range(len(p[row]) - 1, -1, -1):
+            if p[row][col] == p_token:
+                tokens = 1
+                r_index, c_index = row, col
+                while r_index - 1 >= 0 and c_index - 1 >= 0:
+                    r_index -= 1
+                    c_index -= 1
+                    if p[r_index][c_index] == p_token:
+                        tokens += 1
+                        if check_diagonal_win(tokens):
+                            return True
+                    else:
+                        break
+    
+    return False
